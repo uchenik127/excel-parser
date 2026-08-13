5286cfb10f1f8097042448899875ada7c90b11d8
uchenik127 <ponomarevdima2007@gmail.com>
2026-08-13 22:50:34 +0300

    Научился считать длинну столбцов и повторил фильтрацию данных

--- parser.py
+++ parser.py
@@ -1,24 +1,19 @@
 import pandas as pd
-import os
-os.makedirs("input",exist_ok = True)
-os.makedirs("output",exist_ok = True)
-files = [f for f in os.listdir("input") if f.endswith(".xlsx")]
-if not files:
-    print("❌ В папке input нет .xlsx файлов!")
-    print("Положите файлы и запустите снова.")
-else:
-    print(f"📁 Найдено файлов: {len(files)}")
-    all_data = []
-    for file in files:
-        input_path = os.path.join("input",file)
-        print(f"  📄 Читаю: {file}")
-        df = pd.read_excel(input_path)
-        df["Источник"] = file
-        all_data.append(df)
-    merged_df = pd.concat(all_data,ignore_index = True)
-    merged_df = merged_df[(merged_df["Статус"] != "Отменен") | (merged_df["Город"] != "Омск")]
-    output_path = os.path.join("output","merged_report.xlsx")
-    merged_df.to_excel(output_path,index = False)
-    print(f"\n✅ Готово! Объединено {len(files)} файлов.")
-    print(f"✅ Всего строк: {len(merged_df)}")
-    print(f"✅ Сохранено: {output_path}")
+df = pd.read_excel("parsing_Otdelka_fasada_kamnem.xlsx")
+def include_words(text):
+    text = str(text).lower()
+    if "натуральный" in text and "камень" in text:
+        return True
+    else:
+        return False
+
+df.to_excel("parsing_Otdelka_fasada_kamnem.xlsx")
+
+df["Целевая"]=df["Запрос"].apply(include_words)
+df_target = df[df["Целевая"] == True]
+df_target = df_target.drop(columns=["Целевая"])
+df_target["Длина запроса"] = df_target["Запрос"].str.len()
+df_sorted = df_target.sort_values("Длина запроса",ascending=False)
+df_sorted.to_excel("filtered_by_natural.xlsx",index=False)
+print(f"✅ Осталось запросов: {len(df_sorted)}")
+print("✅ Сохранено в filtered_by_natural.xlsx")
