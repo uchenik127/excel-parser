7526aa314a70edc740a0ff20b09ca0fc72c5ac22
uchenik127 <ponomarevdima2007@gmail.com>
2026-08-16 18:46:36 +0300

    excepts

--- parser.py
+++ parser.py
@@ -1,34 +1,30 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
-url1 = "https://habr.com/ru/feed/"
-url2 = "https://habr.com/ru/feed/page2/"
-def all_titels_and_links(url):
-    response = requests.get(url)
-    if response.status_code != 200:
-        print("Ошибка")
-        return [],[]
-    soup = BeautifulSoup(response.text)
-    items = soup.find_all( "h2",class_="tm-title tm-title_h2 title")
+url = "https://habr.com/ru/feed/"
+try:
+    response = requests.get(url,timeout= 5)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, "html.parser")
+    items = soup.find_all("a", class_="tm-title__link")
     titles = []
     linkes = []
     for item in items:
-        find = item.find("a")
-        if find:
-            titles.append(find.text)
-            linkes.append(find.get("href"))
-    return titles, linkes
-titles1,linkes1 = all_titels_and_links(url1)
-print(f'Получено {len(titles1)} строк')
-titles2,linkes2 = all_titels_and_links(url2)
-print(f'Получено {len(titles2)} строк')
-titles1.extend(titles2)
-linkes1.extend(linkes2)
-df = pd.DataFrame({
-    "Заголовок" : titles1,
-    "Ссылка" : linkes1
-})
-df_filtred = df[df["Заголовок"].str.contains("Рынок|Цены",case = False)]
-df_sorted =df_filtred.sort_values("Заголовок",ascending=False)
-df_clean = df_sorted.drop_duplicates("Заголовок",keep="first")
-df_clean.to_excel("habr.xlsx",index=False)
+        titles.append(item.text)
+        linkes.append(item.get("href"))
+    df = pd.DataFrame({
+        "Заголовок" : titles,
+        "Ссылка" : linkes
+    })
+    df_filtred = df[df["Заголовок"].str.contains('вайбкодим|правда',case = False)]
+    df_sorted = df_filtred.sort_values("Заголовок",ascending= False)
+    df_clean = df_sorted.drop_duplicates("Заголовок",keep= "first")
+    df_clean.to_excel("habr_all_filtred.xlsx",index= False)
+except requests.HTTPError:
+    print("Ошибка сайт не отвечает")
+except requests.Timeout:
+    print("Ошибка сайт слишком долго грузит")
+except requests.ConnectionError:
+    print("Ошибка не удалось подключится к сайту")
+except Exception as e:
+    print(f"Ошибка {e}")
