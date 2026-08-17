9a0289dc106666d854e4827508d7f18faf946dbd
uchenik127 <ponomarevdima2007@gmail.com>
2026-08-17 15:18:47 +0300

    add pages

--- parser.py
+++ parser.py
@@ -1,30 +1,43 @@
-import pandas as pd
-from bs4 import BeautifulSoup
 import requests
-url = "https://habr.com/ru/feed/"
-try:
-    response = requests.get(url,timeout= 5)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, "html.parser")
-    items = soup.find_all("a", class_="tm-title__link")
-    titles = []
-    linkes = []
-    for item in items:
-        titles.append(item.text)
-        linkes.append(item.get("href"))
-    df = pd.DataFrame({
-        "Заголовок" : titles,
-        "Ссылка" : linkes
-    })
-    df_filtred = df[df["Заголовок"].str.contains('вайбкодим|правда',case = False)]
-    df_sorted = df_filtred.sort_values("Заголовок",ascending= False)
-    df_clean = df_sorted.drop_duplicates("Заголовок",keep= "first")
-    df_clean.to_excel("habr_all_filtred.xlsx",index= False)
-except requests.HTTPError:
-    print("Ошибка сайт не отвечает")
-except requests.Timeout:
-    print("Ошибка сайт слишком долго грузит")
-except requests.ConnectionError:
-    print("Ошибка не удалось подключится к сайту")
-except Exception as e:
-    print(f"Ошибка {e}")
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+base_url = "https://habr.com/ru/feed/"
+all_page = 6
+titeles = []
+links = []
+pages = []
+for page in range(1,all_page):
+    if page == 1:
+        url = "https://habr.com/ru/feed/"
+    else:
+        url = f"https://habr.com/ru/feed/page{page}/"
+    try:
+        time.sleep(1)
+        response = requests.get(url,timeout=5)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, "html.parser")
+        items = soup.find_all("a",class_ = "tm-title__link")
+        for item in items:
+            titeles.append(item.text)
+            links.append(item.get("href"))
+            pages.append(page)
+
+    except requests.Timeout:
+        print("Ошибка сервер долго грузится")
+        break
+    except requests.ConnectionError:
+        print("Ошибка не получилось загрузить сервер")
+        break
+    except Exception as e:
+        print(f"Ошибка {e}")
+        break
+df = pd.DataFrame({
+    "Страница" : pages,
+    "Заголовок": titeles,
+    "Ссылка": links
+})
+df_filtered = df[df["Заголовок"].str.contains("Код|Айти", case = False)]
+df_clean = df_filtered.drop_duplicates("Заголовок",keep="first")
+df_sorted =df_clean.sort_values("Заголовок",ascending= False)
+df_sorted.to_excel("habr_page.xlsx",index=False)
